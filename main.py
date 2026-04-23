1cacfa88d50f6306d6ece61b3166c12f732c235f
Shaxriyor2003zz-ui <Shaxriyor2003zz@icloud.com>
2026-04-23 16:29:59 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from aiogram import Bot, Dispatcher, types
+import asyncio
+
+BOT_TOKEN = "SIZNING_TOKEN"
+CHANNEL_ID = "@sTsmarketpalace"
+
+bot = Bot(8235644002:AAGR6uaVjMVBuncwS_mqvSThLGjrp7MsobI)
+dp = Dispatcher()
+
+AUTO_TEXT = """
+
+👤 Admin: @RF_shakhr
+📞 Telefon: +998 91 778 26 81
+📦 Tezkor yetkazib berish
+
+Buyurtma uchun admin bilan bog‘laning ✨
+"""
+
+@dp.message()
+async def post_to_channel(message: types.Message):
+    if message.photo:
+        await bot.send_photo(
+            chat_id=CHANNEL_ID,
+            photo=message.photo[-1].file_id,
+            caption=(message.caption or "") + AUTO_TEXT
+        )
+    elif message.video:
+        await bot.send_video(
+            chat_id=CHANNEL_ID,
+            video=message.video.file_id,
+            caption=(message.caption or "") + AUTO_TEXT
+        )
+    else:
+        await bot.send_message(
+            chat_id=CHANNEL_ID,
+            text=(message.text or "") + AUTO_TEXT
+        )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
